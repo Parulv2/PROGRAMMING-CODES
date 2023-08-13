56c805a3b9de0a7cefc8ef7833704bd3eed6cc34
PARUL YADAV <139161481+Parulv2@users.noreply.github.com>
2023-08-13 17:33:53 +0530

    Add files via upload

---
+++ INTEGER ARRAY.c
@@ -0,0 +1,15 @@
+// Program in c to input an integers array from the user and display the same
+
+#include<stdio.h>
+int main()
+{
+	int a[100];
+	int i,n;
+	printf(" Enter the size of the integers array ");
+	scanf("%d",&n);
+	printf(" Enter the value of the integer array ");
+	for(i=0; i<=n-1; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+}
